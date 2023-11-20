c07a947fa5f5f23b4f4c7ce13426e74b387f2117
kheira-lakhezoum <146708665+kheira-lakhezoum@users.noreply.github.com>
2023-11-20 23:20:25 +0100

    jour01/plus-loin

---
+++ jour01/plus-loin/plus-loin.py
@@ -0,0 +1,7 @@
+chaine = input("Entrez une chaîne de caractères : ")
+
+if 'e' in chaine:
+    print("La chaîne contient le caractère 'e'.")
+    
+else:
+    print("La chaîne ne contient pas le caractère 'e'.")
