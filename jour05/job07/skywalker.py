ff32312e513849baae6499925ac6133f25c3e529
kheira-lakhezoum <146708665+kheira-lakhezoum@users.noreply.github.com>
2023-12-08 14:40:19 +0100

    jour05

--- jour05/job07/skywalker.py
+++ jour05/job07/skywalker.py
@@ -1,14 +1,28 @@
-# Luke Skywalker, un professeur de Math, fait passer un test et décide de noter ses élèves
-# sur une échelle allant de 0 à 100 inclus.
-# Si un étudiant obtient moins de 40 sur 100, il échoue au test.
-# S'il a plus de 40, il réussit le test. Luke est un professeur fort sympathique et décide
-# donc d’arrondir à la hausse les notes des étudiants ayant réussi le test. Mais Luke n’est
-# quand même pas trop gentil. Cet arrondi à la hausse ne bénéficiera qu’aux étudiants
-# remplissant certains critères, car tout de même, il ne faut pas exagérer, sans blague.
-# Le critère est simple : Si un étudiant a eu une note de moins de strictement 3 points de
-# son prochain multiple de 5, alors sa note est arrondie à ce multiple de 5. Par exemple,
-# un 83 sera arrondi à 85 alors qu’un 82 restera un 82.
+import math
 
-# Pour simplifier le travail de Luke, écrivez une fonction qui prend en paramètre une liste
-# de notes et qui renvoie une liste de notes, arrondies comme il se doit, quand cela est
-# nécessaire.
+notes =  list(range(101))
+l=(15, 24, 49, 72, 83)
+
+
+def note (l): 
+    for i in range (len(l)):
+        if l[i] > 40 :
+            print (f"Pour une note de {l[i]}, le test est reussi")
+
+        else:
+            print (f"Pour une note de {l[i]} le test est un echec")
+note(l)
+
+
+def arrondi (note_initiale):
+    return math.ceil(((note_initiale) - 3) / 5) * 5
+
+note_initiale = len(l)
+
+l_note_arrondie = [arrondi(note) for note in l]
+
+print(f"Note initiale : {l}")
+print(f"Note arrondie : {l_note_arrondie}")
+
+
+
